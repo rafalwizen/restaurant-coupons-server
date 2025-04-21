2b823c0f7a3b545147a1eb455e48d189719f6dd5
Rafal Wizen <rafal.wizen@gmail.com>
2025-04-21 18:11:33 +0200

    Initial commit

---
+++ src/main/java/com/rafalwizen/restaurantcouponsserver/RestaurantCouponsServerApplication.java
@@ -0,0 +1,13 @@
+package com.rafalwizen.restaurantcouponsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCouponsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantCouponsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafalwizen/restaurantcouponsserver/RestaurantCouponsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalwizen.restaurantcouponsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCouponsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
